60b2a3da004bcc729a5c345d30ed1c0e2f206154
singhrakesh73 <singhrakesh73@gmail.com>
2025-09-14 19:28:10 +0530

    Using restTemplate.exchange() method

--- src/main/java/com/example/demo/controller/controller/UserController.java
+++ src/main/java/com/example/demo/controller/controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.demo.controller.controller;
 
 import com.example.demo.controller.pojo.Response;
 import com.example.demo.controller.pojo.User;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,25 +30,30 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<List<Response>> getUsers() {
         String url = "https://jsonplaceholder.typicode.com/users";
-        User[] users = restTemplate.getForObject(url, User[].class);
-        List<Response> responseList = new ArrayList<>();
-        List<Response> list = Arrays.stream(users).map(u -> {
-            Response response = new Response();
-            response.setId(u.getId());
-            if (u.getAddress() != null && u.getAddress().getGeo() != null) {
-                response.setLatitude(u.getAddress().getGeo().getLat());
-                response.setLongitude(u.getAddress().getGeo().getLng());
-            }
-            if (u.getCompany() != null) {
-                response.setCompanyName(u.getCompany().getName());
-            }
-            return response;
-        }).collect(Collectors.toList());
-        return ResponseEntity.ok(list);
+        ResponseEntity<User[]> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, User[].class);
+
+        if(responseEntity.getStatusCodeValue()  == HttpStatus.OK.value()) {
+            User[] users = responseEntity.getBody();
+            List<Response> list = Arrays.stream(users).map(u -> {
+                Response response = new Response();
+                response.setId(u.getId());
+                if (u.getAddress() != null && u.getAddress().getGeo() != null) {
+                    response.setLatitude(u.getAddress().getGeo().getLat());
+                    response.setLongitude(u.getAddress().getGeo().getLng());
+                }
+                if (u.getCompany() != null) {
+                    response.setCompanyName(u.getCompany().getName());
+                }
+                return response;
+            }).collect(Collectors.toList());
+            return ResponseEntity.ok(list);
+        } else {
+            throw new RuntimeException();
+        }
     }
 
-    @ExceptionHandler(RestClientException.class)
-    public ResponseEntity<String> handleRestClientException(RestClientException ex) {
-        return new ResponseEntity<>("Error occurred while making REST call", HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleRestClientException(Exception ex) {
+        return new ResponseEntity<String>("Error occurred while making REST call", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
